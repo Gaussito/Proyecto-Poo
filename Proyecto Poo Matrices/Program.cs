934c9f8e9ab9cac86af18af72d3795efa964d11a
Gaussito <andresmister45@gmail.com>
2026-09-05 13:37:38 -0500

    Add files via upload

---
+++ Proyecto Poo Matrices/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Taller33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el valor de m: ");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el valor de n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el valor de p: ");
+            int p = int.Parse(Console.ReadLine());
+
+            Matrices matrices = new Matrices(m, n, p);
+
+            matrices.LlenarA();
+            matrices.LlenarB();
+
+            Console.WriteLine();
+            matrices.MostrarA();
+
+            Console.WriteLine();
+            matrices.MostrarB();
+
+            matrices.Multiplicar();
+
+            Console.WriteLine();
+            matrices.MostrarC();
+        }
+    }
+}
